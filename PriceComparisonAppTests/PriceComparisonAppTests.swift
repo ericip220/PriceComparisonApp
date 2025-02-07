40a58ef3599f37ed267a6e7b0e5d372a2c53d111
ericip220 <eric_or_bear@yahoo.com>
2025-02-07 11:03:45 +0000

    Initial Commit

---
+++ PriceComparisonAppTests/PriceComparisonAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PriceComparisonAppTests.swift
+//  PriceComparisonAppTests
+//
+//  Created by Eric IP on 07/02/2025.
+//
+
+import Testing
+@testable import PriceComparisonApp
+
+struct PriceComparisonAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
